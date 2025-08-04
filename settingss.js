e8d89669fd60e41e8e84e6128320366b36540b21
Jamesboom1980 <jokosenumikehinde301@gmail.com>
2025-08-04 10:18:02 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: " ",  // add yourmalvin~k6cUjZ6I#SfTtJeCB7YUb3ONs6_1pE_Px7fF6i4qG7Yuh55szjNk Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
